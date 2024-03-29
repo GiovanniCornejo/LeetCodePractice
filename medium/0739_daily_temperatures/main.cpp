c128727ed8ac139e8c1f88900bbcd39a1d33ff91
Gio <60558512+GiovanniCornejo@users.noreply.github.com>
2024-03-28 21:54:07 -0400

    Finished Daily Temperatures

---
+++ medium/0739_daily_temperatures/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <vector>
+#include "Solution.h"
+
+struct Test
+{
+    std::vector<int> input;
+    std::vector<int> expected;
+};
+
+int main()
+{
+    // Create an instance of the Solution class
+    Solution solution;
+
+    // Define test cases
+    std::vector<Test> tests = {
+        {{73, 74, 75, 71, 69, 72, 76, 73},
+         {1, 1, 4, 2, 1, 1, 0, 0}},
+        {{30, 40, 50, 60}, {1, 1, 1, 0}},
+        {{30, 60, 90}, {1, 1, 0}}};
+
+    // Check if solution passed
+    std::string results;
+
+    // Loop over test cases
+    for (const Test &test : tests)
+    {
+        std::vector<int> result = solution.dailyTemperatures(test.input);
+        bool passed = true; // Flag to track if the test case passed or failed
+        if (test.expected.size() != result.size())
+            passed = false;
+        else
+        {
+            for (int i = 0; i < test.expected.size(); ++i)
+            {
+                if (result[i] != test.expected[i])
+                {
+                    passed = false;
+                    break;
+                }
+            }
+        }
+        results += (passed) ? "." : "F";
+
+        std::cout << "Input: [" << test.input[0];
+        for (int i = 1; i < test.expected.size(); ++i)
+            std::cout << ", " << test.expected[i];
+        std::cout << "]\n";
+        std::cout << "\tExpected: [" << test.expected[0];
+        for (int i = 1; i < test.expected.size(); ++i)
+            std::cout << ", " << test.expected[i];
+        std::cout << "]\n";
+        std::cout << "\tResult:   [" << result[0];
+        for (int i = 1; i < result.size(); ++i)
+            std::cout << ", " << result[i];
+        std::cout << "]\n";
+        std::cout << "----------------------------------------" << std::endl;
+    }
+
+    // Summary
+    if (results.find('F') == std::string::npos)
+    {
+        std::cout << "All tests passed." << std::endl;
+    }
+    else
+    {
+        std::cout << results << std::endl;
+        std::cout << "Tests failed." << std::endl;
+    }
+
+    return 0;
+}
